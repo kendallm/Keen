bb5d50b77e872c01da9c4ae4498853343ba76c18
kendallm <1841529+kendallm@users.noreply.github.com>
2020-07-27 21:42:52 -0700

    Format with black

--- iron_master.py
+++ iron_master.py
@@ -2,9 +2,11 @@ import os
 
 from keen import Keen
 
-if __name__ == '__main__':
-  
-  with Keen() as keen:
-    to_number = os.environ['TWILIO_TO_NUMBER']
-    keen.eye('https://www.ironmaster.com/products/quick-lock-adjustable-dumbbells-75/')
-    keen.eye('https://www.ironmaster.com/products/super-bench-pro/')
+if __name__ == "__main__":
+
+    with Keen() as keen:
+        to_number = os.environ["TWILIO_TO_NUMBER"]
+        keen.eye(
+            "https://www.ironmaster.com/products/quick-lock-adjustable-dumbbells-75/"
+        )
+        keen.eye("https://www.ironmaster.com/products/super-bench-pro/")
--- keen.py
+++ keen.py
@@ -4,7 +4,7 @@ import time
 from pathlib import Path
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,61 +12,56 @@ from selenium.webdriver.common.by import By
 from twilio.rest import Client
 
 
-class Keen():
-  
-  def __init__(self):
-    chrome_options = Options()  
-    chrome_options.add_argument("--headless")
-    self.browser = webdriver.Chrome(options=chrome_options)
-    self.to_number = os.environ['TWILIO_TO_NUMBER']
+class Keen:
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        self.browser = webdriver.Chrome(options=chrome_options)
+        self.to_number = os.environ["TWILIO_TO_NUMBER"]
 
-  def eye(self, link, element_name='add-to-cart'):
-    self.browser.get(link)
-    try:
-        notify = not self._check_if_found_before(link)
-        add_to_cart_element = WebDriverWait(self.browser, 5).until(
-            EC.presence_of_element_located((By.NAME, element_name))
-        )
-        if notify:
-            self._mark_link_found(link)
-            message = f'Link: {link}\n Available for purchase'
-            self.notify(self.to_number, message)
-        print(f'Found: {link}')
-    except TimeoutException:
-        pass
+    def eye(self, link, element_name="add-to-cart"):
+        self.browser.get(link)
+        try:
+            notify = not self._check_if_found_before(link)
+            add_to_cart_element = WebDriverWait(self.browser, 5).until(
+                EC.presence_of_element_located((By.NAME, element_name))
+            )
+            if notify:
+                self._mark_link_found(link)
+                message = f"Link: {link}\n Available for purchase"
+                self.notify(self.to_number, message)
+            print(f"Found: {link}")
+        except TimeoutException:
+            pass
 
-  def _check_if_found_before(self, link):
-    try:
-        with open('.found') as f:
-            for line in f.readlines():
-                if line.strip() == link:
-                    return True
-    except FileNotFoundError:
-        Path('.found').touch()
-        
-    return False
+    def _check_if_found_before(self, link):
+        try:
+            with open(".found") as f:
+                for line in f.readlines():
+                    if line.strip() == link:
+                        return True
+        except FileNotFoundError:
+            Path(".found").touch()
 
-  def _mark_link_found(self, link):
-    with open('.found', 'w+') as f:
-        f.writelines([link])
-    
-  def __enter__(self):
-    return Keen()
-    
-  def __exit__(self, type, value, traceback):
-    self.cleanup()
-    
-  def cleanup(self):
-    self.browser.quit()
+        return False
 
-  def notify(self, to_number, message):
-    account_sid = os.environ['TWILIO_ACCOUNT_SID']
-    auth_token = os.environ['TWILIO_AUTH_TOKEN']
-    from_number = os.environ['TWILIO_FROM_NUMBER']
-    
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-                        body=message,
-                        from_=from_number,
-                        to=to_number
-                    )
+    def _mark_link_found(self, link):
+        with open(".found", "w+") as f:
+            f.writelines([link])
+
+    def __enter__(self):
+        return Keen()
+
+    def __exit__(self, type, value, traceback):
+        self.cleanup()
+
+    def cleanup(self):
+        self.browser.quit()
+
+    def notify(self, to_number, message):
+        account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+        auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+        from_number = os.environ["TWILIO_FROM_NUMBER"]
+
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(body=message, from_=from_number, to=to_number)
